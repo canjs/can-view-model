10737691a74756e5783324f72e93c3639fb41f16
David Luecke <daff@neyeon.com>
2016-04-26 10:03:19 -0700

    3.0.0-pre.1

---
+++ dist/cjs/can-view-model.js
@@ -0,0 +1,20 @@
+/*can-view-model@3.0.0-pre.0#can-view-model*/
+var domData = require('can-util/dom/data/data');
+var CanMap = require('can-map');
+module.exports = function (el, attr, val) {
+    var scope = domData.get.call(el, 'viewModel');
+    if (!scope) {
+        scope = new CanMap();
+        domData.set.call(el, 'viewModel', scope);
+    }
+    switch (arguments.length) {
+    case 0:
+    case 1:
+        return scope;
+    case 2:
+        return scope.attr(attr);
+    default:
+        scope.attr(attr, val);
+        return el;
+    }
+};
