f9fd7fbde4a6275caa178c9a1d0efbc2e809397c
Justin Meyer <justinbmeyer@gmail.com>
2016-05-25 16:47:01 -0500

    3.0.0-pre.3

---
+++ dist/amd/can-view-model.js
@@ -0,0 +1,28 @@
+/*can-view-model@3.0.0-pre.2#can-view-model*/
+define(function (require, exports, module) {
+    'use strict';
+    var domData = require('can-util/dom/data');
+    var SimpleMap = require('can-simple-map');
+    var types = require('can-util/js/types');
+    module.exports = function (el, attr, val) {
+        var scope = domData.get.call(el, 'viewModel');
+        if (!scope) {
+            scope = types.DefaultMap ? new types.DefaultMap() : new SimpleMap();
+            domData.set.call(el, 'viewModel', scope);
+        }
+        switch (arguments.length) {
+        case 0:
+        case 1:
+            return scope;
+        case 2:
+            return 'attr' in scope ? scope.attr(attr) : scope[attr];
+        default:
+            if ('attr' in scope) {
+                scope.attr(attr, val);
+            } else {
+                scope[attr] = val;
+            }
+            return el;
+        }
+    };
+});
---
+++ dist/cjs/can-view-model.js
@@ -0,0 +1,26 @@
+/*can-view-model@3.0.0-pre.2#can-view-model*/
+'use strict';
+var domData = require('can-util/dom/data/data');
+var SimpleMap = require('can-simple-map');
+var types = require('can-util/js/types/types');
+module.exports = function (el, attr, val) {
+    var scope = domData.get.call(el, 'viewModel');
+    if (!scope) {
+        scope = types.DefaultMap ? new types.DefaultMap() : new SimpleMap();
+        domData.set.call(el, 'viewModel', scope);
+    }
+    switch (arguments.length) {
+    case 0:
+    case 1:
+        return scope;
+    case 2:
+        return 'attr' in scope ? scope.attr(attr) : scope[attr];
+    default:
+        if ('attr' in scope) {
+            scope.attr(attr, val);
+        } else {
+            scope[attr] = val;
+        }
+        return el;
+    }
+};
