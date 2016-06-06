4b368783335551721fcac0918a5d8eff070f2762
Matthew Phillips <matthew@matthewphillips.info>
2016-06-06 14:15:52 -0400

    3.0.0-pre.4

---
+++ dist/cjs/can-view-model.js
@@ -0,0 +1,27 @@
+/*can-view-model@3.0.0-pre.3#can-view-model*/
+'use strict';
+var domData = require('can-util/dom/data/data');
+var SimpleMap = require('can-simple-map');
+var types = require('can-util/js/types/types');
+var ns = require('can-util/namespace');
+module.exports = ns.viewModel = function (el, attr, val) {
+    var scope = domData.get.call(el, 'viewModel');
+    if (!scope) {
+        scope = types.DefaultMap ? new types.DefaultMap() : new SimpleMap();
+        domData.set.call(el, 'viewModel', scope);
+    }
+    switch (arguments.length) {
+    case 0:
+    case 1:
+        return scope;
+    case 2:
+        return 'attr' in scope ? scope.attr(attr) : scope[attr];
+    default:
+        if ('attr' in scope) {
+            scope.attr(attr, val);
+        } else {
+            scope[attr] = val;
+        }
+        return el;
+    }
+};
