66cd9e5545c3c48e8086225f5ca033f3049a26d8
David Luecke <daff@neyeon.com>
2016-10-14 16:24:50 -0700

    3.0.0

---
+++ dist/amd/can-view-model.js
@@ -0,0 +1,29 @@
+/*can-view-model@3.0.0-pre.8#can-view-model*/
+define(function (require, exports, module) {
+    'use strict';
+    var domData = require('can-util/dom/data');
+    var SimpleMap = require('can-simple-map');
+    var types = require('can-util/js/types');
+    var ns = require('can-util/namespace');
+    module.exports = ns.viewModel = function (el, attr, val) {
+        var scope = domData.get.call(el, 'viewModel');
+        if (!scope) {
+            scope = types.DefaultMap ? new types.DefaultMap() : new SimpleMap();
+            domData.set.call(el, 'viewModel', scope);
+        }
+        switch (arguments.length) {
+        case 0:
+        case 1:
+            return scope;
+        case 2:
+            return 'attr' in scope ? scope.attr(attr) : scope[attr];
+        default:
+            if ('attr' in scope) {
+                scope.attr(attr, val);
+            } else {
+                scope[attr] = val;
+            }
+            return el;
+        }
+    };
+});
