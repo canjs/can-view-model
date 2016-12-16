b284da9909a96fc62d4535b6b36020f98b29c374
Kevin Phillips <kphillips86@gmail.com>
2016-12-15 20:52:41 -0600

    3.1.3

---
+++ dist/amd/can-view-model.js
@@ -0,0 +1,31 @@
+/*can-view-model@3.1.2#can-view-model*/
+define(function (require, exports, module) {
+    'use strict';
+    var domData = require('can-util/dom/data');
+    var SimpleMap = require('can-simple-map');
+    var types = require('can-types');
+    var ns = require('can-namespace');
+    var getDocument = require('can-util/dom/document');
+    module.exports = ns.viewModel = function (el, attr, val) {
+        el = typeof el === 'string' ? getDocument().querySelector(el) : el;
+        var scope = domData.get.call(el, 'viewModel');
+        if (!scope) {
+            scope = types.DefaultMap ? new types.DefaultMap() : new SimpleMap();
+            domData.set.call(el, 'viewModel', scope);
+        }
+        switch (arguments.length) {
+        case 0:
+        case 1:
+            return scope;
+        case 2:
+            return 'attr' in scope ? scope.attr(attr) : scope[attr];
+        default:
+            if ('attr' in scope) {
+                scope.attr(attr, val);
+            } else {
+                scope[attr] = val;
+            }
+            return el;
+        }
+    };
+});
