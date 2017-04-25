28d461613aed16a82a9de158290574cb2ce082d5
cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2017-04-25 23:43:24 +0100

    edit the test and get rid of can-jquery deps

--- can-view-model_test.js
+++ can-view-model_test.js
@@ -2,7 +2,6 @@ var QUnit = require('steal-qunit');
 var viewModel = require('can-view-model');
 var SimpleMap = require('can-simple-map');
 var types = require('can-types');
-var $ = require("can-jquery");
 
 QUnit.module('can-view-model');
 
@@ -28,15 +27,18 @@ QUnit.test('a selector can be passed as the first argument (#6)', function(){
 	types.DefaultMap = DefaultMap;
 });
 
-QUnit.test('Allow passing jquery element', function(){
+QUnit.test('Allow passing array like (jQuery) element', function(){
 	var DefaultMap = types.DefaultMap;
+	var $el = {};
 	types.DefaultMap = undefined;
 	var el = document.createElement('div');
 	el.className = 'the-el';
+	$el[0] = el;
+	$el.length = 1;
 	document.getElementById('qunit-fixture').appendChild(el);
-	viewModel($('.the-el'), 'foo', 'bar');
+	viewModel($el, 'foo', 'bar');
 	QUnit.equal(viewModel('.the-el', 'foo'), 'bar', 'It reads view scope from html element');
-	QUnit.equal(viewModel($('.the-el'), 'foo'), 'bar', 'It reads view scope from jquery element');
+	QUnit.equal(viewModel($el, 'foo'), 'bar', 'It reads view scope from array like (jQuery) element');
 	QUnit.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
 	types.DefaultMap = DefaultMap;
 });
