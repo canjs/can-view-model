4bf7f020ef2bb32a2b5128ddc2ef746c56899f7c
Mohamed Cherif Bouchelaghem <cherifbouchelaghem@yahoo.fr>
2017-05-19 17:23:21 +0100

    Allow passing jquery element (#26)
    * allowing jquery element to be passed
    
    * allowing jquery element to be passed
    
    * edit the test and get rid of can-jquery deps
    
    * remove can-jquery from package.json dev deps

--- can-view-model_test.js
+++ can-view-model_test.js
@@ -26,3 +26,19 @@ QUnit.test('a selector can be passed as the first argument (#6)', function(){
 	QUnit.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
 	types.DefaultMap = DefaultMap;
 });
+
+QUnit.test('Allow passing array like (jQuery) element', function(){
+	var DefaultMap = types.DefaultMap;
+	var $el = {};
+	types.DefaultMap = undefined;
+	var el = document.createElement('div');
+	el.className = 'the-el';
+	$el[0] = el;
+	$el.length = 1;
+	document.getElementById('qunit-fixture').appendChild(el);
+	viewModel($el, 'foo', 'bar');
+	QUnit.equal(viewModel('.the-el', 'foo'), 'bar', 'It reads view scope from html element');
+	QUnit.equal(viewModel($el, 'foo'), 'bar', 'It reads view scope from array like (jQuery) element');
+	QUnit.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
+	types.DefaultMap = DefaultMap;
+});
