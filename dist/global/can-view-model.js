01127f3ead06be57786d89c3274b32624ce0065d
Bradley Momberger <brad@bitovi.com>
2017-09-28 10:25:47 -0400

    3.5.0

---
+++ dist/global/can-view-model.js
@@ -0,0 +1,187 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval) {
+	// jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name) {
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for (i = 0; i < parts.length; i++) {
+			if (!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val) {
+		var parts = name.split("."),
+			cur = global,
+			i,
+			part,
+			next;
+		for (i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if (!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod) {
+		if (!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, default: true };
+		for (var p in mod) {
+			if (!esProps[p]) return false;
+		}
+		return true;
+	};
+
+	var hasCjsDependencies = function(deps) {
+		return (
+			deps[0] === "require" && deps[1] === "exports" && deps[2] === "module"
+		);
+	};
+
+	var modules =
+		(global.define && global.define.modules) ||
+		(global._define && global._define.modules) ||
+		{};
+	var ourDefine = (global.define = function(moduleName, deps, callback) {
+		var module;
+		if (typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for (i = 0; i < deps.length; i++) {
+			args.push(
+				exports[deps[i]]
+					? get(exports[deps[i]])
+					: modules[deps[i]] || get(deps[i])
+			);
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if (hasCjsDependencies(deps) || (!deps.length && callback.length)) {
+			module = { exports: {} };
+			args[0] = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args[1] = module.exports;
+			args[2] = module;
+		} else if (!args[0] && deps[0] === "exports") {
+			// Babel uses the exports and module object.
+			module = { exports: {} };
+			args[0] = module.exports;
+			if (deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if (!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if (globalExport && !get(globalExport)) {
+			if (useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	});
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function() {
+		// shim for @@global-helpers
+		var noop = function() {};
+		return {
+			get: function() {
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load) {
+				doEval(__load.source, global);
+			}
+		};
+	});
+})(
+	{},
+	typeof self == "object" && self.Object == Object ? self : window,
+	function(__$source__, __$global__) {
+		// jshint ignore:line
+		eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+	}
+);
+
+/*can-view-model@3.4.0#can-view-model*/
+define('can-view-model', [
+    'require',
+    'exports',
+    'module',
+    'can-util/dom/data/data',
+    'can-simple-map',
+    'can-types',
+    'can-namespace',
+    'can-util/dom/document/document',
+    'can-util/js/is-array-like/is-array-like',
+    'can-reflect'
+], function (require, exports, module) {
+    'use strict';
+    var domData = require('can-util/dom/data/data');
+    var SimpleMap = require('can-simple-map');
+    var types = require('can-types');
+    var ns = require('can-namespace');
+    var getDocument = require('can-util/dom/document/document');
+    var isArrayLike = require('can-util/js/is-array-like/is-array-like');
+    var canReflect = require('can-reflect');
+    module.exports = ns.viewModel = function (el, attr, val) {
+        var scope;
+        if (typeof el === 'string') {
+            el = getDocument().querySelector(el);
+        } else if (isArrayLike(el) && !el.nodeType) {
+            el = el[0];
+        }
+        if (canReflect.isObservableLike(attr) && canReflect.isMapLike(attr)) {
+            return domData.set.call(el, 'viewModel', attr);
+        }
+        scope = domData.get.call(el, 'viewModel');
+        if (!scope) {
+            scope = types.DefaultMap ? new types.DefaultMap() : new SimpleMap();
+            domData.set.call(el, 'viewModel', scope);
+        }
+        switch (arguments.length) {
+        case 0:
+        case 1:
+            return scope;
+        case 2:
+            return 'attr' in scope ? scope.attr(attr) : scope[attr];
+        default:
+            if ('attr' in scope) {
+                scope.attr(attr, val);
+            } else {
+                scope[attr] = val;
+            }
+            return el;
+        }
+    };
+});
+/*[global-shim-end]*/
+(function(global) { // jshint ignore:line
+	global._define = global.define;
+	global.define = global.define.orig;
+}
+)(typeof self == "object" && self.Object == Object ? self : window);
