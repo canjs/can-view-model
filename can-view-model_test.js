9da4104893752e0b755b15716526494ca398490f
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 18:54:01 +0100

    Landscaper: QUnit2 upgrade

--- can-view-model_test.js
+++ can-view-model_test.js
@@ -4,36 +4,36 @@ var SimpleMap = require('can-simple-map');
 
 QUnit.module('can-view-model');
 
-QUnit.test('basics', function(){
+QUnit.test('basics', function(assert) {
 	var el = document.createElement('div');
 	viewModel(el, 'foo', 'bar');
-	QUnit.equal(viewModel(el,'foo'), 'bar');
-	QUnit.ok(viewModel(el) instanceof SimpleMap, 'is SimpleMap');
+	assert.equal(viewModel(el,'foo'), 'bar');
+	assert.ok(viewModel(el) instanceof SimpleMap, 'is SimpleMap');
 });
 
-QUnit.test('a selector can be passed as the first argument (#6)', function(){
+QUnit.test('a selector can be passed as the first argument (#6)', function(assert) {
 	var el = document.createElement('div');
 	el.className = 'the-el';
 	document.getElementById('qunit-fixture').appendChild(el);
 	viewModel('.the-el', 'foo', 'bar');
-	QUnit.equal(viewModel('.the-el', 'foo'), 'bar');
-	QUnit.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
+	assert.equal(viewModel('.the-el', 'foo'), 'bar');
+	assert.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
 });
 
-QUnit.test('set custom can-simple-map on element (#5)', function(){
+QUnit.test('set custom can-simple-map on element (#5)', function(assert) {
 	var vm, elVm;
 	var CustomMap = SimpleMap.extend({});
 	var el = document.createElement('div');
 	document.getElementById('qunit-fixture').appendChild(el);
 	vm = new CustomMap({ foo: 'bar' });
 	elVm = viewModel(el, vm);
-	QUnit.equal(viewModel(el,'foo'), 'bar');
+	assert.equal(viewModel(el,'foo'), 'bar');
 });
 
 
 
 
-QUnit.test('Allow passing array like (jQuery) element', function(){
+QUnit.test('Allow passing array like (jQuery) element', function(assert) {
 
 	var $el = {};
 
@@ -43,18 +43,18 @@ QUnit.test('Allow passing array like (jQuery) element', function(){
 	$el.length = 1;
 	document.getElementById('qunit-fixture').appendChild(el);
 	viewModel($el, 'foo', 'bar');
-	QUnit.equal(viewModel('.the-el', 'foo'), 'bar', 'It reads view scope from html element');
-	QUnit.equal(viewModel($el, 'foo'), 'bar', 'It reads view scope from array like (jQuery) element');
-	QUnit.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
+	assert.equal(viewModel('.the-el', 'foo'), 'bar', 'It reads view scope from html element');
+	assert.equal(viewModel($el, 'foo'), 'bar', 'It reads view scope from array like (jQuery) element');
+	assert.ok(viewModel(el) instanceof SimpleMap, 'is can-map');
 });
 
-QUnit.test('elements with length property not treated as arraylikes (#31)', function() {
+QUnit.test('elements with length property not treated as arraylikes (#31)', function(assert) {
 	var el = document.createElement('select');
 	document.getElementById('qunit-fixture').appendChild(el);
-	QUnit.equal(el.length, 0, "Select has length property (0 for empty)");
-	QUnit.deepEqual(viewModel(el).get(), {}, "viewModel created on empty select");
+	assert.equal(el.length, 0, "Select has length property (0 for empty)");
+	assert.deepEqual(viewModel(el).get(), {}, "viewModel created on empty select");
 	var opt = document.createElement('option');
 	el.appendChild(opt);
-	QUnit.equal(el.length, 1, "Select has length 1");
-	QUnit.deepEqual(viewModel(el).get(), {}, "viewModel created on non-empty select");
+	assert.equal(el.length, 1, "Select has length 1");
+	assert.deepEqual(viewModel(el).get(), {}, "viewModel created on non-empty select");
 });
